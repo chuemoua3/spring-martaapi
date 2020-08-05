f612af314884ce642e76fa8f57fb0becdc90ddb9
chuemoua3 <chuemoua12@outlook.com>
2020-08-05 00:12:08 -0400

    working transit app

--- src/main/resources/static/custom.js
+++ src/main/resources/static/custom.js
@@ -1,23 +1,47 @@
 var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: {
-      lat: parseFloat(busLocations[0].LATITUDE),
-      lng: parseFloat(busLocations[0].LONGITUDE),
-    },
-    zoom: 15,
-    scrollwheel: false,
-  });
-
-  for (i = 0; i < busLocations.length; i++) {
-    var marker = new google.maps.Marker({
-      position: {
-        lat: parseFloat(busLocations[i].LATITUDE),
-        lng: parseFloat(busLocations[i].LONGITUDE),
-      },
-      map: map,
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: parseFloat(busLocations[0].LATITUDE), lng: parseFloat(busLocations[0].LONGITUDE) },
+        zoom: 15,
+        scrollwheel: false
     });
-  }
+	
+    for (i=0; i<busLocations.length; i++){
+        var marker = new google.maps.Marker({
+            position: { lat: parseFloat(busLocations[i].LATITUDE), lng: parseFloat(busLocations[i].LONGITUDE) },
+            map: map,
+            icon: 'https://findicons.com/files/icons/951/google_maps/32/bus.png'
+        });
+    }
+
+    infoWindow = new google.maps.InfoWindow;
+
+      // Try HTML5 geolocation.
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent('You are here!!!');
+          infoWindow.open(map);
+          map.setCenter(pos);
+        }, function() {
+          handleLocationError(true, infoWindow, map.getCenter());
+        });
+      } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+      }
 }
 
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+            'Error: The Geolocation service failed.' :
+            'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+  }
 
